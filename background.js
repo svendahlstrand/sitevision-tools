e1ce87b11f0b0188e01426460cafc8e5e4e94180
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2013-02-26 10:47:20 +0100

    Close current tab and highlight editor tab when user tries to open a second editor.

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
+function onBeforeNavigate(details) {
+  var newTabHost = details.url.replace(/^https?:\/\/([^\/]+)(.*)/, '$1');
+
+  chrome.tabs.query({ title: 'SiteVision Server editor' }, function (tabs) {
+    tabs.forEach(function (tab) {
+      var host = tab.url.replace(/^https?:\/\/([^\/]+)(.*)/, '$1');
+
+      if (tab.id != details.tabId && host == newTabHost) {
+        chrome.tabs.remove(details.tabId);
+        chrome.tabs.highlight({ tabs: tab.index }, function () {});
+      }
+    });
+  });
+}
+
 function onMessage(message, sender, sendResponse) {
   chrome.pageAction.show(sender.tab.id);
 
@@ -5,3 +20,4 @@ function onMessage(message, sender, sendResponse) {
 }
 
 chrome.extension.onMessage.addListener(onMessage);
+chrome.webNavigation.onBeforeNavigate.addListener(onBeforeNavigate, { url: [{ pathPrefix: '/editor' }] });
